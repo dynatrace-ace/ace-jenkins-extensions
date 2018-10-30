346bedf9c0d9eb6e2e0a00046e6dbc68186e337b
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2018-10-30 16:06:53 +0100

    Storing .tlf file in results dir.

--- vars/executeJMeter.groovy
+++ vars/executeJMeter.groovy
@@ -55,7 +55,7 @@ def call( Map args )
 
     // lets run the test and put the console output to output.txt
     echo "Execute the jMeter test and console output goes to output.txt."
-    sh "/jmeter/bin/jmeter.sh -n -t ./${scriptName} -e -o ${resultsDir} -l result.tlf -JSERVER_URL='${serverUrl}' -JDT_LTN='${LTN}' -JVUCount='${vuCount}' -JLoopCount='${loopCount}' -JCHECK_PATH='${checkPath}' -JSERVER_PORT='${serverPort}' -JThinkTime='${thinkTime}' > output.txt"                    
+    sh "/jmeter/bin/jmeter.sh -n -t ./${scriptName} -e -o ${resultsDir} -l ${resultsDir}/result.tlf -JSERVER_URL='${serverUrl}' -JDT_LTN='${LTN}' -JVUCount='${vuCount}' -JLoopCount='${loopCount}' -JCHECK_PATH='${checkPath}' -JSERVER_PORT='${serverPort}' -JThinkTime='${thinkTime}' > output.txt"                    
     sh "cat output.txt"
 
     // archive the artifacts
